58836c5de853c42c1e02113716e05a312bb58b06
Pedro Ayres <pedroayresb@gmail.com>
2022-11-22 19:19:26 -0300

    conserta 3 e 4?

--- src/middlewares/products.js
+++ src/middlewares/products.js
@@ -6,7 +6,7 @@ const productValidate = (req, res, next) => {
   }
 
   if (name.length < 5) {
-    return res.status(400).json({ message: '"name" length must be at least 5 characters long' });
+    return res.status(422).json({ message: '"name" length must be at least 5 characters long' });
   }
 
   next();
