f076cffacc6ed0f14d566fa5cf66d370135deff8
Pedro Ayres <pedroayresb@gmail.com>
2022-11-22 16:52:36 -0300

    conserta 1?

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -8,7 +8,7 @@ const getProducts = (req, res) => {
 const getProductById = (req, res) => {
   const { id } = req.params;
   const { type, message } = productsServices.getById(id);
-  if (type === 'notFound') return res.status(404).json({ message });
+  if (type) return res.status(404).json({ message });
   return res.status(200).json(message);
 };
 
