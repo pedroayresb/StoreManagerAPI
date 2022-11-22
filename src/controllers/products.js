68aa1eabd8a49bf5990a8b2d7ed0b6d3664e5e27
Pedro Ayres <pedroayresb@gmail.com>
2022-11-22 18:24:18 -0300

    adiciona async await

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,13 +1,13 @@
 const productsServices = require('../services/products');
 
-const getProducts = (req, res) => {
-  const { message } = productsServices.getAll();
+const getProducts = async (req, res) => {
+  const { message } = await productsServices.getAll();
   res.status(200).json(message);
 };
 
-const getProductById = (req, res) => {
+const getProductById = async (req, res) => {
   const { id } = req.params;
-  const { type, message } = productsServices.getById(id);
+  const { type, message } = await productsServices.getById(id);
   if (type) return res.status(404).json({ message });
   return res.status(200).json(message);
 };
