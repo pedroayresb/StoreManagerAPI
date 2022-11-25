e12891856a076feba3fd9a04d503fbe4a0998471
Pedro Ayres <pedroayresb@gmail.com>
2022-11-25 16:13:29 -0300

    conserta 16 de vdd agora

--- src/routes/sales.js
+++ src/routes/sales.js
@@ -10,6 +10,6 @@ router
   .get('/', salesController.getAllSales)
   .get('/:id', validateSaleId, salesController.getSaleById)
   .delete('/:id', validateSaleId, salesController.deleteSaleById)
-  .put('/:id', validateSaleId, salesController.updateSaleById);
+  .put('/:id', validateSaleProduct, validateSaleQuantity, salesController.updateSaleById);
 
 module.exports = router;
